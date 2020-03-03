1896e6a8ccd47431238e04372e57a0d22d72b4d1
qhwa <qhwa@163.com>
2020-03-03 09:06:28 +0800

    fix HSTS

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :bonfire, BonfireWeb.Endpoint,
   http: [port: 4000],
   url: [host: "bonfirereading.com", port: 443, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: :hsts
+  force_ssl: [hsts: true]
 
 # Do not print debug messages in production
 config :logger, level: :info
