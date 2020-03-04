ea854966e1ba2e4eb8de49e29c01d5d84087e4ad
qhwa <qhwa@163.com>
2020-03-04 15:38:00 +0800

    add new book will not cause redirecting any more

--- lib/bonfire_web/live/book_suggestion.ex
+++ lib/bonfire_web/live/book_suggestion.ex
@@ -12,19 +12,24 @@ defmodule BonfireWeb.Live.BookSuggestion do
 
   def render(assigns) do
     ~L"""
+    <% flash = live_flash(@flash, :info) %>
+    <%= if flash do %>
+    <div class="alert alert-info" phx-click="lv:clear-flash" phx-value-key="info"><%= flash %></div>
+    <% end %>
+
     <%= form_tag "#", phx_change: :user_input, class: "suggestions" do %>
       <input type="text" id="q" name="q" autocomplete="off" phx-debounce="500" />
       <ul>
         <%= for book <- @books, book.isbn do %>
         <li>
           <div class="cover">
-          <%= link to: "#", phx_click: "select", phx_value_isbn: book.isbn do %>
+          <%= link to: "#", phx_click: "select", phx_value_isbn: book.isbn, phx_value_isbn: book.title do %>
           <img src="<%= book.thumbnail %>" />
           <% end %>
           </div>
 
           <div class="info">
-            <div class="title"><%= link book.title, to: "#", phx_click: "select", phx_value_isbn: book.isbn, class: "title" %></div>
+            <div class="title"><%= link book.title, to: "#", phx_click: "select", phx_value_isbn: book.isbn, phx_value_title: book.title, class: "title" %></div>
             <div class="subtitle"><%= book.subtitle %></div>
             <div class="authors"><%= book.authors %></div>
           </div>
@@ -53,13 +58,13 @@ defmodule BonfireWeb.Live.BookSuggestion do
     {:noreply, assign(socket, :books, books)}
   end
 
-  def handle_event("select", %{"isbn" => isbn}, socket) do
+  def handle_event("select", %{"isbn" => isbn, "title" => title}, socket) do
     case Tracks.create_reading_state(%{"isbn" => isbn, "user_id" => user_id(socket)}) do
       :ok ->
-        {:noreply,
-         socket
-         |> put_flash(:info, "reading tracked")
-         |> redirect(to: Routes.reading_state_path(BonfireWeb.Endpoint, :index))}
+        {:noreply, socket |> put_flash(:info, "New book (#{title}) added!")}
+
+      {:error, :already_reading} ->
+        {:noreply, socket |> put_flash(:info, "You have already added it.")}
 
       error ->
         Logger.error(["Fail on creating reading track, reason: ", inspect(error)])
