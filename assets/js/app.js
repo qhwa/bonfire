c36536a788ca03fc129f0174590adf981ede8dce
qhwa <qhwa@163.com>
2020-03-07 21:52:40 +0800

    improve layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import "bulma/css/bulma.css";
+import "../css/app.css";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -9,14 +10,14 @@ import css from "../css/app.css"
 //
 // Import dependencies
 //
-import "phoenix_html"
+import "phoenix_html";
 
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
-import {Socket} from "phoenix"
-import LiveSocket from "phoenix_live_view"
+import {Socket} from "phoenix";
+import LiveSocket from "phoenix_live_view";
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
 const liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}});
