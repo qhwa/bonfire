aee17b3961d065d7bf73b2fbc4d87ff6c4e0b722
qhwa <qhwa@163.com>
2020-03-13 20:35:46 +0800

    improve UI

--- lib/bonfire/sharing.ex
+++ lib/bonfire/sharing.ex
@@ -12,7 +12,7 @@ defmodule Bonfire.Sharing do
   def get_reading_states_by_profile(share_key) do
     case Repo.get_by(Profile, share_key: share_key) do
       %{user_id: user_id} ->
-        {:ok, Bonfire.Tracks.list_reading_states(user_id)}
+        {:ok, user_id, Bonfire.Tracks.list_reading_states(user_id)}
 
       nil ->
         {:error, :not_found}
