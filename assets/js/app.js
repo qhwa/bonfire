e8446586cbf3905376d7a9a09e7ef40692cf81d8
qhwa <qhwa@163.com>
2020-03-19 07:29:22 +0800

    improve UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "balloon-css";
 import "../css/bulma.sass";
 import "../css/app.css";
 
