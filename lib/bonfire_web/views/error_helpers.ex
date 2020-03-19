37c64eabb8cfc390b425ae0d7377ad7db40be48d
qhwa <qhwa@163.com>
2020-03-19 18:13:31 +0800

    fix bugs on editing profiles

--- lib/bonfire_web/views/error_helpers.ex
+++ lib/bonfire_web/views/error_helpers.ex
@@ -10,8 +10,8 @@ defmodule BonfireWeb.ErrorHelpers do
   """
   def error_tag(form, field) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
-      content_tag(:span, translate_error(error),
-        class: "help-block",
+      content_tag(:p, translate_error(error),
+        class: "help is-danger",
         data: [phx_error_for: input_id(form, field)]
       )
     end)
