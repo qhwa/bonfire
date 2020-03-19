3f1d76da74ba1d2aabf5d01418fe26512a81d7a1
qhwa <qhwa@163.com>
2020-03-19 08:33:09 +0800

    add basic structure of profile editing

--- lib/bonfire_web/controllers/profile_controller.ex
+++ lib/bonfire_web/controllers/profile_controller.ex
@@ -2,7 +2,11 @@ defmodule BonfireWeb.ProfileController do
   use BonfireWeb, :controller
 
   alias Bonfire.Sharing
+  alias Bonfire.Sharing.Profile
   alias Bonfire.Tracks
+
+  plug Pow.Plug.RequireAuthenticated,
+       [error_handler: Pow.Phoenix.PlugErrorHandler] when action in [:edit]
 
   action_fallback BonfireWeb.FallbackController
 
@@ -13,5 +17,15 @@ defmodule BonfireWeb.ProfileController do
       |> put_layout("sharing.html")
       |> render("show.html", reading_states: reading_states, stats: stats)
     end
+  end
+
+  def edit(conn, _params) do
+    with {:ok, profile} <- load_profile(conn.assigns.current_user) do
+      render(conn, "edit.html", profile: Profile.updating_changeset(profile, %{}))
+    end
+  end
+
+  defp load_profile(user) do
+    Bonfire.Users.get_or_create_profile(user.id)
   end
 end
